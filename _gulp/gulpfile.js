1707e94cc9a254ffd290340c3f59a14e0c7c15ea
yuki-funami <shipwebd@gmail.com>
2024-10-18 18:18:17 +0900

    gulpfile.jsの画像圧縮およびピクパの修正

--- _gulp/gulpfile.js
+++ _gulp/gulpfile.js
@@ -113,25 +113,33 @@ const cssSass = () => {
 const imgImagemin = () => {
 // 変更があった画像のみ処理対象にし、複数の保存先に対応する
 return src(srcPath.img)
-.pipe(changed(destPath.img)) // 最初の保存先で変更を検出
-.pipe(
-imagemin(
-[
-imageminMozjpeg({ quality: 80 }), // JPEG画像の圧縮
-imageminPngquant({quality: [0.7, 0.8]}), // PNG画像の圧縮
-imageminSvgo({ plugins: [{ removeViewbox: false }] }), // SVG画像の圧縮
-],
-{ verbose: true }
-)
-)
-.pipe(dest(destPath.img)) // 最初の保存先に保存
-.pipe(webp()) // webpに変換
-.pipe(dest(destPath.img)) // webpを最初の保存先に保存
-.pipe(src(srcPath.img)) // 再度画像ソースを読み込み
-.pipe(changed(destWpPath.img)) // WordPress用の保存先で変更を検出
-.pipe(dest(destWpPath.img)) // WordPress用の保存先に保存
-.pipe(webp()) // webpに変換
-.pipe(dest(destWpPath.img)); // webpをWordPress用の保存先に保存
+  .pipe(changed(destPath.img)) // 最初の保存先で変更を検出
+  .pipe(
+    imagemin([
+      imageminMozjpeg({ quality: 80 }), // JPEG画像の圧縮
+      imageminPngquant({quality: [0.7, 0.8]}), // PNG画像の圧縮
+      imageminSvgo({ plugins: [{ removeViewbox: false }] }), // SVG画像の圧縮
+    ],
+    { verbose: true }
+  ))
+  .pipe(dest(destPath.img)) // 最初の保存先に保存
+  .pipe(webp()) // webpに変換
+  .pipe(dest(destPath.img)) // webpを最初の保存先に保存
+
+  // WordPress用の保存先への処理
+  .pipe(src(srcPath.img)) // 再度画像ソースを読み込み
+  .pipe(changed(destWpPath.img)) // WordPress用の保存先で変更を検出
+  .pipe(
+    imagemin([
+      imageminMozjpeg({ quality: 80 }), // JPEG画像の圧縮
+      imageminPngquant({quality: [0.7, 0.8]}), // PNG画像の圧縮
+      imageminSvgo({ plugins: [{ removeViewbox: false }] }), // SVG画像の圧縮
+    ],
+    { verbose: true }
+  ))
+  .pipe(dest(destWpPath.img)) // WordPress用の保存先に保存
+  .pipe(webp()) // webpに変換
+  .pipe(dest(destWpPath.img)); // webpをWordPress用の保存先に保存
 };
 
 // js圧縮
